effe56de21759d38832136ac8aec8503c52dbae3
Xander Low <xandy@Xanders-MacBook-Pro.local>
2022-01-06 18:00:16 -0800

    chore: complete sentry integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from 'theme/index'
+import PlausibleProvider from 'next-plausible';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL!}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </PlausibleProvider>
   );
 }
 
