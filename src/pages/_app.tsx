45185a9e89648f782a0a8e318c227f5d43793e8e
Xander Low <alexanderjameslow@gmail.com>
2022-01-07 13:27:52 -0800

    added: DialogContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from 'theme/index'
-import PlausibleProvider from 'next-plausible';
+import PlausibleProvider from 'next-plausible'
+import { DialogProvider } from 'context/dialogContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL!}>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <DialogProvider>
+          <Component {...pageProps} />
+        </DialogProvider>
       </ChakraProvider>
     </PlausibleProvider>
-  );
+  )
 }
 
 export default MyApp
