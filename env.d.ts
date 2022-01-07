9e4ca16e6010f4a9bc27d92001a03667efc93150
Griko Nibras <git@griko.id>
2022-01-08 04:20:07 +0700

    feat: add process.env value types
    Signed-off-by: Griko Nibras <git@griko.id>

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
-// custom type declarations file
+// make sure to sync with .env.example file
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly SENTRY_DSN: string
+    readonly NEXT_PUBLIC_SENTRY_DSN: string
+  }
+}
