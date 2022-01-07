30674185d128d845277c218443bdea1d4acf1e6e
Xander Low <xander@xandydandy.com>
2022-01-07 15:04:03 -0800

    removed duplicated import from merge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { DialogProvider } from 'context/dialogContext'
 import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
 import theme from 'theme/index'
-import PlausibleProvider from 'next-plausible'
-import { DialogProvider } from 'context/dialogContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
